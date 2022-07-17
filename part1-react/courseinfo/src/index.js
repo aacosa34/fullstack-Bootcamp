c1e197a24aa563394f8f47410a2272a4e8671c6b
Adrian Acosa Sanchez <adrianacosa@correo.ugr.es>
2022-07-17 14:09:39 +0200

    Exercise 1.5 done

--- part1-react/courseinfo/src/index.js
+++ part1-react/courseinfo/src/index.js
@@ -6,7 +6,7 @@ const Header = ({course}) => <h1>{course}</h1>
 */
 
 const Header = (props) => {
-  return <h1>{props.course}</h1>
+  return <h1>{props.course['name']}</h1>
 }
 
 // One-liner version of Content component. Props are overrided by
@@ -33,27 +33,29 @@ const Total = ({ parts }) => {
 const Part = ({ part }) => <p>{part['name']} {part['exercises']}</p>
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course['parts']} />
+      <Total parts={course['parts']} />
     </div>
   )
 }
